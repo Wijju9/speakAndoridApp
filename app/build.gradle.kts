4bcecedde56efa8b079aa22bd810f2aeb8a967ca
Wijju9 <77786480+Wijju9@users.noreply.github.com>
2026-03-27 15:40:04 +0530

    Fix resource linking by using MaterialComponents theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.6.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
     implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
 
     implementation("androidx.room:room-runtime:2.6.1")
